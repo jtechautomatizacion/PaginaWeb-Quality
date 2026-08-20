52a378eb2ca8cf4687b897f16ffaf88d4fd97b5a
Group TQC Dev <geremin94@gmail.com>
2026-08-20 02:18:01 -0500

    Revert "Fix: add CORS headers to backend for production domains"
    This reverts commit c4767416a5b6e7731c0cb18edb02933974b887f6.

--- server.js
+++ server.js
@@ -15,19 +15,6 @@ const CURSOS_PATH = path.join(ROOT, 'cursos.json');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-    const allowedOrigins = ['https://grouptqualityc.com.pe', 'https://api.grouptqualityc.com.pe', 'http://localhost'];
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    if (req.method === 'OPTIONS') return res.sendStatus(204);
-    next();
-});
-
 const BLOCKED_PATHS = [/^\/\.env/i, /^\/\.git(\/|$)/i, /^\/usuarios\.json$/i];
 
 app.use((req, res, next) => {
